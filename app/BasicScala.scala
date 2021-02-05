0b8f5e0b2a92951172b0660e57cee6d65ae1a8e7
K1MB055 <Migricirta25@Yahoo.fr>
2021-02-05 01:13:08 +0100

    réponse

--- app/BasicScala.scala
+++ app/BasicScala.scala
@@ -17,13 +17,14 @@ object BasicScala {
     * input  : Map()
     * output : ""
     */
-  def encodeParamsInUrl(params: Map[String, String]): String = ???
-
+  def encodeParamsInUrl(params: Map[String, String]): String = 
+    params.foldLeft("")( (acc, kv) => acc + "?"+ kv._1 + "=" + kv._2 +"&").dropRight(1)
+    //there is an additional '&' at the end that i have to delete with dropRight(1)
 
   /**
     * Test if a String is an email
     */
-  def isEmail(maybeEmail: String): Boolean = ???
+def isEmail(maybeEmail: String): Boolean = """^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$""".r.unapplySeq(maybeEmail).isDefined  
 
 
   /**
@@ -37,7 +38,18 @@ object BasicScala {
     * input : (i = 99, n = 38997)
     * output : 1723793299
     */
-  def power(i:Int, n:Int):Int = ???
+  def power(i:Int, n:Int):Int = 
+  (i,n) match {
+    case (0,_) => 0
+    case (_,0) => 1
+    case (_,1) => i
+    case (_,n) => if (n % 2 == 0) { ( power (i,(n / 2)) ) * ( power (i,(n / 2)) )} 
+                  else { i * ( power (i, ((n - 1) / 2))) * ( power (i, ((n - 1) / 2)) )}
+  }
 
-
+  def main(args: Array[String]) = {
+    //println(encodeParamsInUrl(Map("sort_by" -> "name", "order_by" -> "asc", "user_id" -> "12")))
+    //println(isEmail("anis@gmailfr"))
+    //println(power(99,38997))
+   }
 }
--- app/WhatsWrong1.scala
+++ app/WhatsWrong1.scala
@@ -3,15 +3,21 @@ package com.particeep.test
 object WhatsWrong1 {
 
   trait Interface {
-    val city: String
+    def city: String //val city: String
     val support: String = s"Ici c'est $city !"
   }
 
   case object Supporter extends Interface {
 
-    override val city = "Paris"
+    //override val city = "Paris"
+    override def city = "Paris"
   }
 
-  Supporter.city //What does this print ?
-  Supporter.support //What does this print and why ? How to fix it ?
+  Supporter.city //What does this print ? (it prints : "Paris")
+  //Supporter.support //What does this print and why ? How to fix it ? (it prints : "Ici c'est null !", 
+  //the reason behind this is that the $city in the variable support is evaluated when definied with the interface variable
+  //city and not with the overrided one from the object Supporter )
+  // fix : use def instead of val 
+  // val : evaluates when defined.
+  // def : evaluates on every call.
 }
