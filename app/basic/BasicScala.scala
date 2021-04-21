1840f41541295ca620e203af8ba9f347c913b5ca
Tomohiro ISHIWATA <tomohiro.ishiwata@gmail.com>
2021-04-21 11:14:31 +0200

    [basic] answers

--- app/basic/BasicScala.scala
+++ app/basic/BasicScala.scala
@@ -1,5 +1,7 @@
 package com.particeep.test.basic
 
+import scala.collection.immutable.{AbstractMap, SeqMap, SortedMap}
+
 /**
  * This is basic language questions so don't use external library or build in function
  */
@@ -16,12 +18,24 @@ object BasicScala {
    * input  : Map()
    * output : ""
    */
-  def encodeParamsInUrl(params: Map[String, String]): String = ???
+  def encodeParamsInUrl(params: Map[String, String]): String = {
+    def encode(params: Map[String, String], acc: String): String = {
+      if(params.isEmpty) acc.substring(0, acc.length-1)
+      else {
+        encode(params.tail, acc + params.head._1 + "=" + params.head._2 + "&")
+      }
+    }
 
+    if(params.isEmpty) ""
+    else encode(params, "?")
+  }
   /**
    * Test if a String is an email
    */
-  def isEmail(maybeEmail: String): Boolean = ???
+  def isEmail(maybeEmail: String): Boolean = {
+    val emailRegex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,3}$"
+    maybeEmail.matches(emailRegex)
+  }
 
   /**
    * Compute i ^ n
@@ -34,6 +48,15 @@ object BasicScala {
    * input : (i = 99, n = 38997)
    * output : 1723793299
    */
-  def power(i: Int, n: Int): Int = ???
+  def power(i: Int, n: Int): Int = {
+    def power(x: Int, y: Int, acc: Int): Int = {
+      if(y == 0) acc
+      else {
+        power(x, y-1, acc*x)
+      }
+    }
+
+    power(i, n, 1)
+  }
 
 }
