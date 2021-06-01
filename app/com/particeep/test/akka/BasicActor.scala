64ed6851cfbfc8c4ddb32343ced9af504d1323a8
hmagrahi <haikel.magrahi@ingenico.com>
2021-06-01 18:10:50 +0200

    test fix bug

--- app/com/particeep/test/akka/BasicActor.scala
+++ app/com/particeep/test/akka/BasicActor.scala
@@ -1,12 +1,10 @@
 package com.particeep.test.akka
 
 import akka.actor.{ Actor, ActorSystem, Props }
-import akka.pattern.ask
 import akka.util.Timeout
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
-import scala.util.{ Failure, Success }
 
 /**
  * Question about Akka framework http://akka.io
@@ -19,8 +17,8 @@ class BasicActor extends Actor {
   private val HELLO_THERE_MESSAGE = "Hello there."
   private val WHAT_MESSAGE        = "What?"
   private def respond: Receive    = {
-    case HELLO_MESSAGE => sender() ! HELLO_THERE_MESSAGE
-    case _             => sender() ! WHAT_MESSAGE
+    case HELLO_MESSAGE => print(HELLO_THERE_MESSAGE)
+    case _             => print(WHAT_MESSAGE)
   }
 
   override def receive: Receive = respond
@@ -39,15 +37,7 @@ object FireActor {
   def fireActor(): Unit = {
     val system      = ActorSystem("ActorSystem")
     val basic_actor = system.actorOf(Props[BasicActor](), name = "basic_actor")
-    (basic_actor ? "Hello").onComplete {
-      case Success(value: String) => println(value)
-      case Success(value)         => throw new RuntimeException(s"Unknown response $value")
-      case Failure(exception)     => throw exception
-    }
-    (basic_actor ? "BlaBla").onComplete {
-      case Success(value: String) => println(value)
-      case Success(value)         => throw new RuntimeException(s"Unknown response $value")
-      case Failure(exception)     => throw exception
-    }
+    basic_actor ! "Hello"
+    basic_actor ! "BlaBla"
   }
 }
